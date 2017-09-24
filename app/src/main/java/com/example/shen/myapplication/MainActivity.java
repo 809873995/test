2e38abeb0918f27df9b77a77c89072ca20f9d139
shen <shen@shendeMacBook-Pro.local>
2017-09-25 01:46:33 +0800

    first activity

---
+++ app/src/main/java/com/example/shen/myapplication/MainActivity.java
@@ -0,0 +1,17 @@
+package com.example.shen.myapplication;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
+/**
+ * Created by shen on 2017/9/25.
+ */
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
